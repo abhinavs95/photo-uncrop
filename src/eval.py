b1759bef58c953e5c0afdcab46a8bfc6afa4c56e
Aashish Kumar Misraa <iaashish7@gmail.com>
2018-12-03 19:17:39 -0500

    Add files via upload

---
+++ src/eval.py
@@ -0,0 +1,9 @@
+def dist1(img1,img2):
+    return np.abs(img1,img2)
+
+def dist2(img1,img2):
+    return np.sqrt(np.sum((img1-img2)**2))
+
+def psnr(img1,img2):
+    mse = np.mean((img1-img2)**2)
+    return 20 * np.log10(255.0 / np.sqrt(mse))
